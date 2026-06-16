1dfef19c9feda1ff6ecf0dc92d96cedbb66d6e26
Pavan23761A0530 <kommojupavankumarganesh@gmail.com>
2026-06-16 19:27:20 +0530

    enhanced taking input

--- Basicjava/Main.java
+++ Basicjava/Main.java
@@ -5,10 +5,14 @@ import java.util.*;
 class Main {
     public static void main(String args[]) {
         Scanner s = new Scanner(System.in);
+        System.out.println("Enter your name");
         String name = s.nextLine();
+        System.out.println("Enter Roll number");
         int roll_no = s.nextInt();
         s.nextLine();
+        System.out.println("enter your college name");
         String college = s.nextLine();
+        System.out.println("enter cgpa");
         Double cgpa = s.nextDouble();
         System.out.println(name);
         System.out.println(roll_no);
